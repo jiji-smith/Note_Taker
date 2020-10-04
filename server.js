ba5c93a97877c730af924c880bdb88100dc084bc
Ji Smith <jihyunjung0219@gmail.com>
2020-10-04 10:55:54 -0500

    crated API

--- server.js
+++ server.js
@@ -18,6 +18,18 @@ app.get("/notes", function(request,response){
   response.sendFile(path.join(__dirname + '/public', "notes.html"))
 })
 
+//GET /api/notes - Should read the db.json file and return all saved notes as JSON.
+// I created API!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+app.get("/api/notes", function(request,response){
+    console.log(response);
+    fs.readFile(__dirname + "/db/db.json", (error,data) => {
+        if (error) throw error;
+        const notes = JSON.parse(data)
+        return response.json(notes)
+    })
+  });
+
+
 //GET * - Should return the index.html file
 app.get("*", function(request,response){
     console.log(response);
