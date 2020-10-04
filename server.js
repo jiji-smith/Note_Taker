fca9b1216d82762d15648c512cf15f45afd08a8e
Ji Smith <jihyunjung0219@gmail.com>
2020-10-04 10:41:42 -0500

    note route works

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
-
 //set up the server
 const express = require("express")
+const path = require("path")
+const fs = require("fs")
 
 const app = express()
 const PORT = process.env.PORT || 13000
@@ -10,7 +11,12 @@ app.use(express.urlencoded({extended:true}))
 app.use(express.json())
 app.use(express.static(__dirname + '/public'))
 
+app.get("/notes", function(request,response){
+  console.log(response);
+  response.sendFile(path.join(__dirname + '/public', "notes.html"))
+})
 
+//server on
 app.listen(PORT, function(request, response){
     console.log("app listening on port : ", PORT)
 })
