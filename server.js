ecaf60b4ce7b6d21d7c61fab4865a6e2944ae559
Ji Smith <jihyunjung0219@gmail.com>
2020-10-04 10:44:36 -0500

    wild card working

--- server.js
+++ server.js
@@ -11,12 +11,22 @@ app.use(express.urlencoded({extended:true}))
 app.use(express.json())
 app.use(express.static(__dirname + '/public'))
 
+
+//GET /notes - Should return the notes.html file.
 app.get("/notes", function(request,response){
   console.log(response);
   response.sendFile(path.join(__dirname + '/public', "notes.html"))
 })
 
+//GET * - Should return the index.html file
+app.get("*", function(request,response){
+    console.log(response);
+    response.sendFile(path.join(__dirname + '/public', "index.html"))
+  })
+
 //server on
 app.listen(PORT, function(request, response){
     console.log("app listening on port : ", PORT)
 })
+
+
